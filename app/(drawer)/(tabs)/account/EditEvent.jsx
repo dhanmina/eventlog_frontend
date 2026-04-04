18540afe2a1b2e2f887ae7130ca2bf482f5aa41e
CHOCOPNDN <chocopndn@gmail.com>
2026-04-04 16:15:03 +0800

    chore: remove unused admin screens from account tab

--- app/(drawer)/(tabs)/account/EditEvent.jsx
+++
@@ -1,1 +0,0 @@
-export { default } from "../../eventManagement/events/EditEvent";
--- app/(drawer)/(tabs)/account/_layout.jsx
+++ app/(drawer)/(tabs)/account/_layout.jsx
@@ -1,58 +1,18 @@
 import { Stack } from "expo-router";
-import { useEffect, useState } from "react";
-
 import theme from "../../../../constants/theme";
-import { getRoleID } from "../../../../database/queries";
 
-const AccountLayout = () => {
-  const [roleId, setRoleId] = useState(null);
-
-  useEffect(() => {
-    const fetchRoleId = async () => {
-      const id = await getRoleID();
-      setRoleId(id);
-    };
-
-    fetchRoleId();
-  }, []);
-
-  return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: roleId !== 4,
-          headerStyle: { backgroundColor: theme.colors.primary },
-          headerTintColor: theme.colors.secondary,
-          title: "Account",
-        }}
-      />
-      <Stack.Screen
-        name="AddEvent"
-        options={{
-          headerStyle: { backgroundColor: theme.colors.primary },
-          headerTintColor: theme.colors.secondary,
-          headerTitle: "Add Event",
-        }}
-      />
-      <Stack.Screen
-        name="EditEvent"
-        options={{
-          headerStyle: { backgroundColor: theme.colors.primary },
-          headerTintColor: theme.colors.secondary,
-          headerTitle: "Edit Event",
-        }}
-      />
-      <Stack.Screen
-        name="EventsList"
-        options={{
-          headerStyle: { backgroundColor: theme.colors.primary },
-          headerTintColor: theme.colors.secondary,
-          headerTitle: "Editable Events",
-        }}
-      />
-    </Stack>
-  );
-};
+const AccountLayout = () => (
+  <Stack>
+    <Stack.Screen
+      name="index"
+      options={{
+        headerShown: true,
+        headerStyle: { backgroundColor: theme.colors.primary },
+        headerTintColor: theme.colors.secondary,
+        title: "",
+      }}
+    />
+  </Stack>
+);
 
 export default AccountLayout;
